0b4e24779ccdbc7cd6e2bbafae02f3ce0feb0e11
Niandong Xu <tianxu2009@gmail.com>
2018-03-14 10:11:40 -0400

    first working draft

--- konwey.py
+++ konwey.py
@@ -7,8 +7,8 @@ import random
 #vars
 print_queue = queue.Queue() #a queue of tuples, where a tuple consists of final x, and the associated kon
 #in left, out right
-global commands = ['x=x+1','x=x+2','x=x-1']
-seed = [1];
+commands = ['x=x+1','x=x+2','x=x-1']
+seed = [0];
 
 class live (threading.Thread):
 
@@ -19,21 +19,27 @@ class live (threading.Thread):
 
     def run(self):
         x = 1
+        comm = ""
         for num in self.kon:
-            eval(commands[num])
-        threads_queue.appendleft((x, self.kon))
+            comm += commands[num] + "\n"
+        comm += "self.threads_queue.put((x, self.kon))"
+        exec(comm)
 
 def procreate():
-    #while (true)
-    #run kon
-    live(print_queue, seed).start()
-    #after it runs, get info from print_queue
-    final_x, kon = print_queue.get()
-    #for num of x, create a new kon that is slightly modified from old kon
+    kon_queue = [seed]
+    for _ in range(5):
+        print(_)
+        while(len(kon_queue) > 0):
+            live(print_queue, kon_queue.pop()).start()
+        while(not print_queue.empty()):
+            new_x, kon = print_queue.get()
+            if new_x > 0:
+                for __ in range(new_x):
+                    kon_queue.append(mutate(kon))
+            print("X: " + str(new_x) + " - KON: " + str(kon))
+    #run new kon
 
-    #run that
-
-def edit_kon(kon):
+def mutate(kon): #edit_kon
     #40% add, 30% edit, 20% nothing, 10 % delete
     new_kon = list(kon)
     choice = random.randint(0,99)
@@ -43,18 +49,14 @@ def edit_kon(kon):
             new_kon.append(random.randint(0, len(commands)-1))
     elif (choice < 69): #edit
         for _ in range(lines):
-            line = random.randint(len(new_kon))
+            line = random.randint(0, len(new_kon)-1)
             new_kon[line] = random.randint(0, len(commands)-1)
     elif (choice < 79): #delete
         for _ in range(lines):
-            del new_kon[random.randint(0, len(new_kon))]
+            if len(new_kon) > 1:
+                del new_kon[random.randint(0, len(new_kon)-1)]
 
     return new_kon
 
 if __name__ == "__main__":
     procreate()
-
-    #bias
-    value = random.uniform(0, 4)
-    value = math.sqrt(value)
-    value /= 2
